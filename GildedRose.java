09dcb374cad833e1f8c8ee254662c8a4b93e5436
ytkachenkokm33 <152411105+ytkachenkokm33@users.noreply.github.com>
2024-11-05 04:10:37 +0200

    Update GildedRose.java
                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                        if (item.quality > 0) { змінив місцями ці дві умови для зручності в майбутньому

--- GildedRose.java
+++ GildedRose.java
@@ -25,8 +25,8 @@ class GildedRose {
                             }
                         }
                     } else {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (item.quality > 0) {
                         item.quality = item.quality - 1;
                     }
                 }
@@ -41,8 +41,8 @@ class GildedRose {
                         increaseQualityByOne(item);
                 } else {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality > 0) {
-                            if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (item.quality > 0) {
                                 item.quality = item.quality - 1;
                             }
                         }
