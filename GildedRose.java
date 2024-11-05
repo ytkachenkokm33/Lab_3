283307e6b30237d479a866aa841c7a9bbef1d919
ytkachenkokm33 <152411105+ytkachenkokm33@users.noreply.github.com>
2024-11-05 18:36:53 +0200

    Update GildedRose.java
    Прибрав вкладені if-и, та інвертував if, який не помітив

--- GildedRose.java
+++ GildedRose.java
@@ -11,42 +11,40 @@ class GildedRose {
         for (Item item : items) {
             if (item.name.equals("Aged Brie")
                     || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (item.quality < 50) {
+                if (item.quality < 50) {
+                    item.increaseQualityByOne();
+
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        if (item.sellIn < 11) {
                             item.increaseQualityByOne();
-
-                            if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                                if (item.sellIn < 11) {
-                                        item.increaseQualityByOne();
-                                }
-
-                                if (item.sellIn < 6) {
-                                        item.increaseQualityByOne();
-                                }
-                            }
                         }
-                    } else {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.decreasedQualityByOne();
-                }
-            }
 
+                        if (item.sellIn < 6) {
+                            item.increaseQualityByOne();
+                        }
+                    }
+                }
+            } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                item.decreasedQualityByOne();
+            }
             if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                 item.sellIn = item.sellIn - 1;
             }
 
-            if (item.sellIn < 0) {
-                if (item.name.equals("Aged Brie")) {
-                        item.increaseQualityByOne();
-                } else {
-                    if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+
+            if (item.name.equals("Aged Brie")) {
+                if (item.sellIn < 0) {
+                    item.increaseQualityByOne();
+                }
+            } else if (item.sellIn < 0) {
+                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                        item.quality = 0;
+                    } else {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
                             item.decreasedQualityByOne();
                         }
-                    } else {
-                        item.quality = 0;
                     }
                 }
             }
         }
     }
-}
