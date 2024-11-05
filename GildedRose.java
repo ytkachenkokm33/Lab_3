95871ddb4f2770d220cbd8cdf8965b0a959dabee
ytkachenkokm33 <152411105+ytkachenkokm33@users.noreply.github.com>
2024-11-05 03:01:24 +0200

    Update GildedRose.java
    Виніс items[i].quality = items[i].quality + 1; в окремий метод, бо це не красива структура

--- GildedRose.java
+++ GildedRose.java
@@ -12,18 +12,18 @@ class GildedRose {
             if (item.name.equals("Aged Brie")
                     || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality < 50) {
-                            item.quality = item.quality + 1;
+                            increaseQualityByOne(item);
 
                             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                                 if (item.sellIn < 11) {
                                     if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
+                                        increaseQualityByOne(item);
                                     }
                                 }
 
                                 if (item.sellIn < 6) {
                                     if (item.quality < 50) {
-                                        item.quality = item.quality + 1;
+                                        increaseQualityByOne(item);
                                     }
                                 }
                             }
@@ -43,7 +43,7 @@ class GildedRose {
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
                     if (item.quality < 50) {
-                        item.quality = item.quality + 1;
+                        increaseQualityByOne(item);
                     }
                 } else {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
@@ -58,5 +58,9 @@ class GildedRose {
                 }
             }
         }
+    }
+
+    public void increaseQualityByOne(Item item) {
+        item.quality = item.quality + 1;
     }
 }
