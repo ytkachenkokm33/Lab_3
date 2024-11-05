65c4833fe9715f8181fed8b5ec0a6ace4f0074f4
ytkachenkokm33 <152411105+ytkachenkokm33@users.noreply.github.com>
2024-11-05 20:26:08 +0200

    Update GildedRose.java
    Оптимізував першу частину коду для кращої роботи з нею

--- GildedRose.java
+++ GildedRose.java
@@ -9,19 +9,18 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Aged Brie")
-                    || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (item.name.equals("Aged Brie")){
                 if (item.quality < 50) {
                     item.increaseQualityByOne();
-
-                    if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                }
+            } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality < 50) {
+                    item.increaseQualityByOne();
                         if (item.sellIn < 11) {
                             item.increaseQualityByOne();
                         }
-
                         if (item.sellIn < 6) {
                             item.increaseQualityByOne();
-                        }
                     }
                 }
             } else if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
