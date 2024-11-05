5240f031be9251fd2f1a54113f334a31844bed9d
ytkachenkokm33 <152411105+ytkachenkokm33@users.noreply.github.com>
2024-11-05 04:16:18 +0200

    Update GildedRose.java
            if (item.quality > 0) {
                item.quality = item.quality - 1;
            } аналогічно виносимо цю структуру в окремий метод, щоб слідувати правилу "One Dot Per Line" та переносимо в клас item

--- GildedRose.java
+++ GildedRose.java
@@ -12,23 +12,21 @@ class GildedRose {
             if (item.name.equals("Aged Brie")
                     || item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (item.quality < 50) {
-                            increaseQualityByOne(item);
+                            item.increaseQualityByOne();
 
                             if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                                 if (item.sellIn < 11) {
-                                        increaseQualityByOne(item);
+                                        item.increaseQualityByOne();
                                 }
 
                                 if (item.sellIn < 6) {
-                                        increaseQualityByOne(item);
+                                        item.increaseQualityByOne();
                                 }
                             }
                         }
                     } else {
                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    if (item.quality > 0) {
-                        item.quality = item.quality - 1;
-                    }
+                    item.decreasedQualityByOne();
                 }
             }
 
@@ -38,13 +36,11 @@ class GildedRose {
 
             if (item.sellIn < 0) {
                 if (item.name.equals("Aged Brie")) {
-                        increaseQualityByOne(item);
+                        item.increaseQualityByOne();
                 } else {
                     if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            if (item.quality > 0) {
-                                item.quality = item.quality - 1;
-                            }
+                            item.decreasedQualityByOne();
                         }
                     } else {
                         item.quality = 0;
